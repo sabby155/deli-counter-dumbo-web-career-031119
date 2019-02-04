8814ba37da3401d9d7d553d2b97127deef1b5f2e
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-04 20:55:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line (array)
  if array.length == 0
    puts "The line is currently empty."
   end 
- else array.each_with_index {|x,i| puts ""} 
+ else array.each_with_index {|x,i| puts "The line is currently: #{i+1}. #{x}"} end
 end   
 
 
