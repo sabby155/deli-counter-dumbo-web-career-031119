cb7b247c2f9edd62af845ac323626a6832b02a49
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-04 21:29:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,12 @@ katz_deli = []
 def line (array)
   if array.length == 0
     puts "The line is currently empty."
-  end 
   else 
     num = [] 
     array.each_with_index {|x,i| num.push(i+1, x)}
     num = num.join(". ")
     puts "the line is currently: #{num}"
+      end 
 end   
 
 
